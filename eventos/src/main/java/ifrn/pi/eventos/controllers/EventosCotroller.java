a52ee0e2c7e03b8c982b8378fb8ad88db7518a72
Hana Caynã de Lima da Silva <20231204010050@ifrn.local>
2026-09-08 14:13:08 -0300

    Enviando dados do formulário para a aplicação

--- eventos/src/main/java/ifrn/pi/eventos/controllers/EventosCotroller.java
+++ eventos/src/main/java/ifrn/pi/eventos/controllers/EventosCotroller.java
@@ -1,15 +1,27 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosCotroller {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "formEventos";
 		
 	}
+	
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+		return "evento-adicionado";
+	}
+	
 
 }
